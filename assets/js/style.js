58c35b0babf2251116e37158cf9117e6aa5fa46b
koroliov <160582219+Rugachka@users.noreply.github.com>
2024-06-05 18:49:46 +0300

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,9 @@
+
+function checkAnswer() {
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked').value;
+    if (selectedAnswer == 3) {
+        alert("Вірно");
+    } else {
+        alert("Не вірно");
+    }
+}
